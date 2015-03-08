394aec5c37b0477498701ed0030cdffe6b8e40d1
Ivana Rodriguez <ivanaramaaa@gmail.com>
2015-03-08 15:25:06 -0400

    Add logout page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'pages/about' => 'pages#about'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
-  
+  get 'logout' => 'sessions#destroy'
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
